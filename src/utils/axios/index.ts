dac87d65e8e08ab370a65e4c04160692b0d8e287
Newo123 <scerep529@gmail.com>
2023-06-27 23:55:35 +0600

    init

---
+++ src/utils/axios/index.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const token = localStorage.getItem('token');
+
+export const instance = axios.create({
+	baseURL: 'http://localhost:3001/api',
+	timeout: 5000,
+	headers: {
+		'X-Custom-Header': 'foobar',
+	},
+});
+
+export const instanceAuth = axios.create({
+	baseURL: 'http://localhost:3001/api',
+	timeout: 5000,
+	headers: {
+		'X-Custom-Header': 'foobar',
+		Authorization: `Bearer ${token}`,
+	},
+});
